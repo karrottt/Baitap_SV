35e9de90564d5881259209e505f9f8162b2af806
karrottt <115169101+karrottt@users.noreply.github.com>
2022-12-23 05:33:39 +0700

    Delete PycharmProjects/pythonProject directory

--- PycharmProjects/pythonProject/Lập trình với tập tin và thư mục/Bài 2/cau2.py
+++
@@ -1,8 +0,0 @@
-#Nhập tên tập tin từ bàn phím
-a = input("Nhập tên tập tin từ bàn phím: ")
-
-#Đọc nội dung tập tin và in ra màn hình
-file = open('D:/data/My Documents/%s.txt' % a, mode = 'r')
-print(file.read(3))
-
-#Ghi danh sách trên vào tập tin theo quy tắc:
